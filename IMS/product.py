4bc7ba59578104222cc482d12841ae06c4fb9b3b
lidacain <lida2004adil@gmail.com>
2023-11-16 19:17:53 +0600

    product class

---
+++ IMS/product.py
@@ -0,0 +1,26 @@
+class Product():
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def get_name(self):
+        return self.name
+
+    def get_price(self):
+        return self.price
+
+    def get_quantity(self):
+        return self.quantity
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_price(self, price):
+        self.price = price
+
+    def set_quantity(self, quantity):
+        self.quantity = quantity
+
+    def __str__(self):
+        return "Name: " + self.name + " Price: " + str(self.price) + " Quantity: " + str(self.quantity)
