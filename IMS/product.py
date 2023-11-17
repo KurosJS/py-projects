b1a05dd19bb2adefadea72930265970845b0b509
lidacain <lida2004adil@gmail.com>
2023-11-17 15:12:33 +0600

    product class

--- IMS/product.py
+++ IMS/product.py
@@ -1,4 +1,8 @@
+import csv
+
 class Product:
+    FILENAME = 'products.csv'
+
     def __init__(self, product_id, name, price, quantity, description):
         self.product_id = product_id
         self.name = name
@@ -9,65 +13,43 @@ class Product:
     def __str__(self):
         return f"Product: {self.name}, price: {self.price}, quantity: {self.quantity}"
 
-    def __repr__(self):
-        return f"Product: {self.name}, price: {self.price}, quantity: {self.quantity}"
-
     def rest(self, quantity):
         self.quantity += quantity
+        self._update_csv()
 
     def sell(self, quantity):
         if self.quantity - quantity < 0:
             raise Exception("Not enough products in stock")
         self.quantity -= quantity
+        self._update_csv()
+
+    def _update_csv(self):
+        with open(Product.FILENAME, mode='r', newline='') as file:
+            reader = csv.DictReader(file)
+            rows = list(reader)
+
+        for row in rows:
+            if int(row['Product ID']) == self.product_id:
+                row['Quantity'] = str(self.quantity)
+
+        with open(Product.FILENAME, mode='w', newline='') as file:
+            fieldnames = ['Product ID', 'Name', 'Price', 'Quantity', 'Description']
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(rows)
+
+    @staticmethod
+    def load_products():
+        products = []
+        with open(Product.FILENAME, mode='r', newline='') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                product = Product(int(row['Product ID']), row['Name'], float(row['Price']), int(row['Quantity']), row['Description'])
+                products.append(product)
+        return products
 
     def get_total_sum(self):
         return self.quantity * self.price
 
     def get_details(self):
         return f"Name: {self.name}, price: {self.price}, quantity: {self.quantity}, description: {self.description}"
-
-
-banana = Product(1, "banana", 20, 100, "yellow")
-apple = Product(2, "apple", 30, 200, "green")
-orange = Product(3, "orange", 40, 300, "orange")
-
-print(banana)
-print(apple)
-print(orange)
-
-print(banana.get_total_sum())
-print(apple.get_total_sum())
-print(orange.get_total_sum())
-
-banana.sell(10)
-apple.sell(20)
-orange.sell(30)
-
-print(banana)
-print(apple)
-print(orange)
-
-print(banana.get_total_sum())
-print(apple.get_total_sum())
-print(orange.get_total_sum())
-
-banana.rest(10)
-apple.rest(20)
-orange.rest(30)
-
-print(banana)
-print(apple)
-print(orange)
-
-print(banana.get_total_sum())
-print(apple.get_total_sum())
-print(orange.get_total_sum())
-
-print(banana.get_details())
-print(apple.get_details())
-print(orange.get_details())
-
-
-
-
-
